a9e7e2ea6e301e33216780a9d8ba8e06eb8ac78c
ali-shah007 <alishah69.ah@gmail.com>
2026-05-08 12:13:26 +0500

    update number

--- src/components/home/WhatsAppButton.jsx
+++ src/components/home/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { MessageCircle } from "lucide-react";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/447775067833"
+      href="https://wa.me/447447712847"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg z-50 transition"
